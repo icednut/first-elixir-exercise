a5adbef448dc13ba90905f402a8b65dae8345f29
icednut <wglee21g@gmail.com>
2023-02-12 09:25:42 +0900

    supervisor study

--- sequence2/lib/sequence2/server.ex
+++ sequence2/lib/sequence2/server.ex
@@ -1,10 +1,10 @@
-defmodule Sequence.Server do
+defmodule Sequence2.Server do
   use GenServer
 
   ############
   # 외부 API
-  def start_link(current_number) do
-    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
   def next_number do
@@ -17,8 +17,8 @@ defmodule Sequence.Server do
 
   #############
   # 젠서버 구현
-  def init(initial_number) do
-    { :ok, initial_number }
+  def init(_) do
+    { :ok, Sequence2.Stash.get() }
   end
 
   def handle_call(:next_number, _from, current_number) do
@@ -29,7 +29,11 @@ defmodule Sequence.Server do
     { :noreply, current_number + delta }
   end
 
-  def format_status(_reason, [ _pdict, state ]) do
-    [data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
+  # def format_status(_reason, [ _pdict, state ]) do
+  #   [data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
+  # end
+
+  def terminate(_reason, current_number) do
+    Sequence2.Stash.update(current_number)
   end
 end
--- stack/lib/stack/server.ex
+++ stack/lib/stack/server.ex
@@ -1,20 +1,57 @@
 defmodule Stack.Server do
   use GenServer
 
+  def start_link(stack) do
+    GenServer.start_link(__MODULE__, stack, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  def push(element) do
+    GenServer.call(__MODULE__, {:push, element})
+  end
+
   def init(initial_array) do
     { :ok, initial_array }
   end
 
-  def handle_call(:pop, _from, []) do
-    { :reply, [], [] }
+  def handle_call(:pop, _from, stack) do
+    next_stack = pop(stack)
+
+    IO.puts("\n[current_stack]")
+    print_stack(stack)
+    IO.puts("\n[next_stack]")
+    print_stack(next_stack)
+    IO.puts("\n[_from] #{inspect _from}")
+
+    { :reply, next_stack, next_stack }
   end
 
-  def handle_call(:pop, _from, [head]) do
-    { :reply, [head], head }
+  def handle_call({:push, element}, _from, stack) do
+    next_stack = [element | stack]
+    { :reply, next_stack, next_stack }
   end
 
-  # TODO tail이 \t일 경우에 대한 처리도 필요함. 이걸 어떻게 분기할까?
-  def handle_call(:pop, _from, [head|tail]) do
-    { :reply, [head|tail], tail }
+  defp pop([]) do
+    IO.puts("비어있음")
+    []
+  end
+  defp pop([head]) do
+    IO.puts("하나만 있음, #{head}")
+    []
+  end
+  defp pop([head | tail]) do
+    IO.puts("head: #{head}")
+    IO.puts("tail:")
+    print_stack(tail)
+    tail
+  end
+
+  defp print_stack(stack) do
+    for elem <- stack do
+      IO.puts("elem: #{elem}")
+    end
   end
 end
